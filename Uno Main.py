ba9d1feb5b69a556c7a23ae9b740df01952df02c
Mark J. B <omnipotentmilk@gmail.com>
2023-11-26 17:30:54 -0500

    Uno Main V5
    Fixed Card_Reader
    Began work on the game loop

--- Uno Main.py
+++ Uno Main.py
@@ -5,7 +5,6 @@ def game_initialize():
         try:
             player_count = int(input("Welcome to UnoPy! Enter Player Number Selection (2-4): "))
             if 2 <= player_count <= 4:
-                print("Selection:", player_count)
                 return player_count
             else:
                 print("Invalid input. Please enter a number between 2 and 4.")
@@ -14,6 +13,8 @@ def game_initialize():
 
 # store the player count to be used for other operations later
 variable_player_count = game_initialize()
+
+
 
 def deck_builder(variable_player_count):
     # create & shuffle the initial UNO deck
@@ -41,26 +42,28 @@ def deck_builder(variable_player_count):
         player1_deck, player2_deck, player3_deck, player4_deck = players_deck_temp
         return player1_deck, player2_deck, player3_deck, player4_deck, uno_deck_instance, players_deck_temp
 
-# store the player decks and the uno deck as a variable to be easily accessible
+
+
+# store the player decks and the uno deck as a variable(s) to be easily accessible
 if variable_player_count == 2:
     player1_deck, player2_deck, remaining_deck, combined_deck_list = deck_builder(variable_player_count)
-    player3_deck = [0]
-    player4_deck = [0]
+    player3_deck = [-1]
+    player4_deck = [-1]
 elif variable_player_count == 3:
     player1_deck, player2_deck, player3_deck, remaining_deck, combined_deck_list = deck_builder(variable_player_count)
-    player4_deck = [0]
+    player4_deck = [-1]
 elif variable_player_count == 4:
     player1_deck, player2_deck, player3_deck, player4_deck, remaining_deck, combined_deck_list = deck_builder(variable_player_count)
 
-playerX_deck = player1_deck
+
 
 # takes a players deck and prints its contents
-def card_reader(playerX_deck):
+def card_reader(input_deck):
     card_info = [
-        "Red 1", "Red 2", "Red 3", "Red 4", "Red 5", "Red 6", "Red 7", "Red 8", "Red 9",
-        "Yellow 1", "Yellow 2", "Yellow 3", "Yellow 4", "Yellow 5", "Yellow 6", "Yellow 7", "Yellow 8", "Yellow 9",
-        "Green 1", "Green 2", "Green 3", "Green 4", "Green 5", "Green 6", "Green 7", "Green 8", "Green 9",
-        "Blue 1", "Blue 2", "Blue 3", "Blue 4", "Blue 5", "Blue 6", "Blue 7", "Blue 8", "Blue 9",
+        "Red 0", "Red 1", "Red 2", "Red 3", "Red 4", "Red 5", "Red 6", "Red 7", "Red 8", "Red 9",
+        "Yellow 0", "Yellow 1", "Yellow 2", "Yellow 3", "Yellow 4", "Yellow 5", "Yellow 6", "Yellow 7", "Yellow 8", "Yellow 9",
+        "Green 0", "Green 1", "Green 2", "Green 3", "Green 4", "Green 5", "Green 6", "Green 7", "Green 8", "Green 9",
+        "Blue 0", "Blue 1", "Blue 2", "Blue 3", "Blue 4", "Blue 5", "Blue 6", "Blue 7", "Blue 8", "Blue 9",
         "Red Special Skip", "Red Special Reverse", "Red Special +2",
         "Yellow Special Skip", "Yellow Special Reverse", "Yellow Special +2",
         "Green Special Skip", "Green Special Reverse", "Green Special +2",
@@ -72,25 +75,39 @@ def card_reader(playerX_deck):
 
     # iterates through each value (n) of the player list, and appends to a new list. this new list rather than having
     # number values has the string values associated with its position in the card_info list.
-    #  if playerX_deck[0] has value 12, it will be "Yellow 4" for example.
-    for n in playerX_deck:
-        output.append(card_info[n])
+    #  if playerX_deck[0] has value 12, it will be "Yellow 1" for example.
+    for n in input_deck:
+        if 0 <= n < len(card_info):
+            output.append(card_info[n])
+        else:
+            output.append(f"Invalid Card, cards number was:{n}") # this was added to debug if any card number
+                                                                 # is messed up because i was having that issue
     print(output)
     return
 
+
+
+# self explanatory checks if length of any deck is 0. if it is that means that player has won
+# true false and 0-4 is simply for future use, unimplemented yet
 def win_condition_check(player1_deck, player2_deck, player3_deck, player4_deck):
     if len(player1_deck) == 0:
-        return True, 0
+        return 0
     elif len(player2_deck) == 0:
-        return True, 1
+        return 1
     elif len(player3_deck) == 0:
-        return True, 2
+        return 2
     elif len(player4_deck) == 0:
-        return True, 3
+        return 3
     else:
-        return False, 4
+        return False
 
+def game_loop(player1_deck, player2_deck, player3_deck, player4_deck, variable_player_count, remaining_deck):
+    current_player = list(range(variable_player_count))
+    remaining_deck_instance = remaining_deck
+    top_card = remaining_deck_instance[0]
 
-card_reader(player1_deck)
+    while win_condition_check(player1_deck, player2_deck, player3_deck, player4_deck) == False:
+        break
+    return
 
-
+game_loop(player1_deck, player2_deck, player3_deck, player4_deck, variable_player_count, remaining_deck)
